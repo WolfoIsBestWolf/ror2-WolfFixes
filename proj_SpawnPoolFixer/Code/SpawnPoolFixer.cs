57c3cb6cb60558b2813cc26adbb8d1801cb3f0ab
Wolfo <max.haldim@gmail.com>
2025-07-21 10:07:38 +0200

    init, split from other repositiories

---
+++ proj_SpawnPoolFixer/Code/SpawnPoolFixer.cs
@@ -0,0 +1,239 @@
+﻿using BepInEx;
+using HG;
+using MonoMod.Cil;
+using R2API.Utils;
+using RoR2;
+using RoR2.ExpansionManagement;
+using RoR2.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+[module: UnverifiableCode]
+
+namespace SpawnPoolFixer
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("Wolfo.DLCSpawnPoolFixer", "DLCSpawnPoolFixer", "1.3.4")]
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
+    public class SotsSpawnPoolFix : BaseUnityPlugin
+    {
+         
+        public void Awake()
+        {
+            WConfig.InitConfig();
+            FixSotsSpawnpools();
+
+            //Make GolemPlains & Roost not twice as common as other stage 1s
+            SceneCollection sgStage1 = Addressables.LoadAssetAsync<SceneCollection>(key: "RoR2/Base/SceneGroups/sgStage1.asset").WaitForCompletion();
+            sgStage1._sceneEntries[0].weight = WConfig.cfgStage1Weight.Value;
+            sgStage1._sceneEntries[1].weight = WConfig.cfgStage1Weight.Value;
+            sgStage1._sceneEntries[2].weight = WConfig.cfgStage1Weight.Value;
+            sgStage1._sceneEntries[3].weight = WConfig.cfgStage1Weight.Value;
+
+            SceneCollection loopSgStage1 = Addressables.LoadAssetAsync<SceneCollection>(key: "RoR2/Base/SceneGroups/loopSgStage1.asset").WaitForCompletion();
+            loopSgStage1._sceneEntries[0].weight = WConfig.cfgStage1Weight.Value;
+            loopSgStage1._sceneEntries[1].weight = WConfig.cfgStage1Weight.Value;
+            loopSgStage1._sceneEntries[2].weight = WConfig.cfgStage1Weight.Value;
+            loopSgStage1._sceneEntries[3].weight = WConfig.cfgStage1Weight.Value;
+
+
+            SceneDirector.onGenerateInteractableCardSelection += FixWrongRadarTowers;
+
+            WolfoFixes.ExtraActions.onMonsterDCCS += RemoveMonsterBasedOnSotVReplacement;
+            //IL.RoR2.ClassicStageInfo.RebuildCards += SotV_EnemyRemovals;
+
+            On.RoR2.BazaarController.IsUnlockedBeforeLooping += NoPreLoopPostLoop;
+
+            //Limit Altar of Gold and Radar Scanner to 1 because, logical
+            Addressables.LoadAssetAsync<InteractableSpawnCard>(key: "32c40c2b1da4a4244871ef499447ac1a").WaitForCompletion().maxSpawnsPerStage = 1;
+            Addressables.LoadAssetAsync<InteractableSpawnCard>(key: "c6c8f501bfa87e54294f9b0bb9db3da4").WaitForCompletion().maxSpawnsPerStage = 1;
+
+            //Lunar Golem remove only being allowed to spawn in Caves
+            //Bad for Dissonance, strange for Commencement, makes them unable to spawn with fixed ITMoon nodes.
+            Addressables.LoadAssetAsync<CharacterSpawnCard>(key: "f7aba48d470b1594281c0eab455a12dd").WaitForCompletion().forbiddenFlags &= ~NodeFlags.NoCeiling;
+
+
+            //All NodeGraphs with 0 NoCeiling Ground spots
+            NodeGraph itancientloft_GroundNodeGraph = Addressables.LoadAssetAsync<NodeGraph>(key: "RoR2/DLC1/itancientloft/itancientloft_GroundNodeGraph.asset").WaitForCompletion();
+            NodeGraph itmoon_GroundNodeGraph = Addressables.LoadAssetAsync<NodeGraph>(key: "RoR2/DLC1/itmoon/itmoon_GroundNodeGraph.asset").WaitForCompletion();
+            NodeGraph voidraid_GroundNodeGraph = Addressables.LoadAssetAsync<NodeGraph>(key: "RoR2/DLC1/voidraid/voidraid_GroundNodeGraph.asset").WaitForCompletion();
+            for (int i = 0; i < itancientloft_GroundNodeGraph.nodes.Length; i++)
+            {
+                itancientloft_GroundNodeGraph.nodes[i].flags |= NodeFlags.NoCeiling;
+            }
+            for (int i = 0; i < itmoon_GroundNodeGraph.nodes.Length; i++)
+            {
+                itmoon_GroundNodeGraph.nodes[i].flags |= NodeFlags.NoCeiling;
+            }
+            for (int i = 0; i < voidraid_GroundNodeGraph.nodes.Length; i++)
+            {
+                voidraid_GroundNodeGraph.nodes[i].flags |= NodeFlags.NoCeiling;
+            }
+  
+        }
+
+   
+
+        private bool NoPreLoopPostLoop(On.RoR2.BazaarController.orig_IsUnlockedBeforeLooping orig, BazaarController self, SceneDef sceneDef)
+        {
+            if (WConfig.cfgLoopSeers.Value == true)
+            {
+                if (sceneDef.loopedSceneDef && Run.instance.stageClearCount >= 5)
+                {
+                    return false;
+                }
+            }
+            return orig(self, sceneDef);
+        }
+
+        private void SotV_EnemyRemovals(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (c.TryGotoNext(MoveType.Before,
+            x => x.MatchStfld("RoR2.ClassicStageInfo", "modifiableMonsterCategories")))
+            {
+                c.EmitDelegate<Func<DirectorCardCategorySelection, DirectorCardCategorySelection>>((dccs) =>
+                {
+                    RemoveMonsterBasedOnSotVReplacement(dccs);
+                    return dccs;
+                });
+            }
+            else
+            {
+                Debug.LogWarning("IL Failed: SotV_EnemyRemovals");
+            }
+        }
+
+        public static void RemoveMonsterBasedOnSotVReplacement(DirectorCardCategorySelection dccs)
+        {
+            if (!WConfig.cfgSotV_EnemyRemovals.Value)
+            {
+                return;
+            }
+            if (Run.instance && Run.instance.IsExpansionEnabled(WolfoFixes.DLCS.DLC1))
+            {
+                string scene = SceneInfo.instance.sceneDef.baseSceneName;
+                switch (scene)
+                {
+                    case "golemplains":
+                        //They remove my guy jellyfish? wtf
+                        RemoveCard(dccs, 2, "fish"); //-> Alpha
+                        break;
+                    case "frozenwall":
+                        RemoveCard(dccs, 2, "Lemurian"); //-> Vermin
+                        RemoveCard(dccs, 2, "Wisp"); //-> Pest
+                        //Reaver
+                        break;
+                    case "dampcavesimple":
+                        RemoveCard(dccs, 1, "GreaterWisp"); //-> Gup
+                        break;
+                    case "shipgraveyard":
+                        RemoveCard(dccs, 2, "Beetle"); //-> Larva
+                        //Reaver
+                        break;
+                    case "rootjungle":
+                        RemoveCard(dccs, 1, "LemurianBruiser"); //-> Gup
+                        RemoveCard(dccs, 2, "Lemurian"); //-> Larva
+                        break;
+                    case "skymeadow":
+                        if (WConfig.WORMREMOVAL.Value)
+                        {
+                            RemoveCard(dccs, 0, "MagmaWorm"); //-> XI
+                        }
+                        //Reaver
+                        break;
+                }
+            }
+        }
+
+        public static void RemoveCard(DirectorCardCategorySelection dccs, int cat, string card)
+        {
+            //Because categories get shifted around in DCCS Blender, better to just find them
+            string a = "";
+            switch (cat)
+            {
+                case 2:
+                    a = "Basic Monsters";
+                    break;
+                case 1:
+                    a = "Minibosses";
+                    break;
+                case 0:
+                    a = "Champions";
+                    break;
+
+            }
+            cat = dccs.FindCategoryIndexByName(a);
+            if (cat != -1)
+            {
+                int c = FindSpawnCard(dccs.categories[cat].cards, card);
+                if (c != -1)
+                {
+                    Debug.Log("SotV Removals : Removed " + card + " from " + SceneInfo.instance.sceneDef);
+                    ArrayUtils.ArrayRemoveAtAndResize(ref dccs.categories[cat].cards, c);
+                    return;
+                }
+            }
+            Debug.LogWarning("Failed to remove " + card + " from " + SceneInfo.instance.sceneDef);
+
+        }
+
+        public static int FindSpawnCard(DirectorCard[] insert, string LookingFor)
+        {
+            for (int i = 0; i < insert.Length; i++)
+            {
+                if (insert[i].spawnCard.name.EndsWith(LookingFor))
+                {
+                    //Debug.Log("Found " + LookingFor);
+                    return i;
+                }
+            }
+            Debug.LogWarning("Couldn't find " + LookingFor);
+            return -1;
+        }
+
+        private void FixWrongRadarTowers(SceneDirector scene, DirectorCardCategorySelection dccs)
+        {
+            int rare = dccs.FindCategoryIndexByName("Rare");
+            if (rare != -1)
+            {
+                var a = dccs.categories[rare];
+                for (int i = 0; i < a.cards.Length; i++)
+                {
+                    if (a.cards[i].forbiddenUnlockableDef)
+                    {
+                        string name = SceneInfo.instance.sceneDef.cachedName;
+                        if (name.StartsWith("habitat"))
+                        {
+                            //This could just be done on every stage, so issues like this NEVER happen again.
+                            UnlockableDef unlockableDef = UnlockableCatalog.GetUnlockableDef("Logs.Stages." + name);
+                            Debug.Log("Automatic Radar Scanner fix for " + unlockableDef);
+                            a.cards[i].forbiddenUnlockableDef = unlockableDef;
+                        }
+                        //Make Scanners more common because grinding for them is stupid
+                        a.cards[i].selectionWeight = 10000;
+                    }
+                }
+            }
+ 
+        }
+
+ 
+        public static void FixSotsSpawnpools()
+        {
+            DccsPool dpArtifactWorld02Monsters = Addressables.LoadAssetAsync<DccsPool>(key: "RoR2/DLC2/artifactworld02/dpArtifactWorld02Monsters.asset").WaitForCompletion();
+            dpArtifactWorld02Monsters.poolCategories[0].includedIfConditionsMet[0].dccs = Addressables.LoadAssetAsync<DirectorCardCategorySelection>(key: "RoR2/DLC2/artifactworld02/dccsArtifactWorld02Monsters_DLC1.asset").WaitForCompletion(); ;
+
+            DirectorCardCategorySelection dccsVillageInteractables_DLC2 = Addressables.LoadAssetAsync<DirectorCardCategorySelection>(key: "RoR2/DLC2/village/dccsVillageInteractables_DLC2.asset").WaitForCompletion();
+            dccsVillageInteractables_DLC2.categories[5].cards[3].minimumStageCompletions = 1;
+        }
+    }
+}
---
+++ proj_WolfoFixes/code/Gameplay/ItemTags.cs
@@ -0,0 +1,245 @@
+﻿using HG;
+using RoR2;
+using UnityEngine.AddressableAssets;
+using UnityEngine;
+
+namespace WolfoFixes
+{
+
+    internal class ItemTags
+    {
+
+        public static ItemTag NoEvolSimu = (ItemTag)94; //Enemy No, Scav Yes, Brother Yes?
+        public static ItemTag NoMonsterScav = (ItemTag)95; //Enemy Yes, Scav No, Brother Yes?
+
+        public static BasicPickupDropTable dtMonsterTeamLunar;
+        public static BasicPickupDropTable dtAISafeBoss;
+
+        public static void Start()
+        {
+            //Using more tags because most of these tags are meant to be AIBlacklisted anyways.
+            //Ie all Sprint -> AiBlacklist but not always tagged as such
+
+            BasicPickupDropTable dtMonsterTeamTier1Item = Addressables.LoadAssetAsync<BasicPickupDropTable>(key: "RoR2/Base/MonsterTeamGainsItems/dtMonsterTeamTier1Item.asset").WaitForCompletion();
+            BasicPickupDropTable dtMonsterTeamTier2Item = Addressables.LoadAssetAsync<BasicPickupDropTable>(key: "RoR2/Base/MonsterTeamGainsItems/dtMonsterTeamTier2Item.asset").WaitForCompletion();
+            BasicPickupDropTable dtMonsterTeamTier3Item = Addressables.LoadAssetAsync<BasicPickupDropTable>(key: "RoR2/Base/MonsterTeamGainsItems/dtMonsterTeamTier3Item.asset").WaitForCompletion();
+            dtMonsterTeamLunar = ScriptableObject.CreateInstance<BasicPickupDropTable>();
+            dtMonsterTeamLunar.name = "dtMonsterTeamLunar";
+            dtMonsterTeamLunar.tier1Weight = 0;
+            dtMonsterTeamLunar.tier2Weight = 0;
+            dtMonsterTeamLunar.tier3Weight = 0;
+            dtMonsterTeamLunar.lunarItemWeight = 1;
+            
+            ArenaMonsterItemDropTable dtArenaMonsterTier1 = Addressables.LoadAssetAsync<ArenaMonsterItemDropTable>(key: "RoR2/Base/arena/dtArenaMonsterTier1.asset").WaitForCompletion();
+            ArenaMonsterItemDropTable dtArenaMonsterTier2 = Addressables.LoadAssetAsync<ArenaMonsterItemDropTable>(key: "RoR2/Base/arena/dtArenaMonsterTier2.asset").WaitForCompletion();
+            ArenaMonsterItemDropTable dtArenaMonsterTier3 = Addressables.LoadAssetAsync<ArenaMonsterItemDropTable>(key: "RoR2/Base/arena/dtArenaMonsterTier3.asset").WaitForCompletion();
+
+            BasicPickupDropTable dtAISafeTier1Item = Addressables.LoadAssetAsync<BasicPickupDropTable>(key: "RoR2/Base/Common/dtAISafeTier1Item.asset").WaitForCompletion();
+            BasicPickupDropTable dtAISafeTier2Item = Addressables.LoadAssetAsync<BasicPickupDropTable>(key: "RoR2/Base/Common/dtAISafeTier2Item.asset").WaitForCompletion();
+            BasicPickupDropTable dtAISafeTier3Item = Addressables.LoadAssetAsync<BasicPickupDropTable>(key: "RoR2/Base/Common/dtAISafeTier3Item.asset").WaitForCompletion();
+            dtAISafeBoss = ScriptableObject.CreateInstance<BasicPickupDropTable>();
+            dtAISafeBoss.name = "dtAISafeBoss";
+            dtAISafeBoss.tier1Weight = 0;
+            dtAISafeBoss.tier2Weight = 0;
+            dtAISafeBoss.tier3Weight = 0;
+            dtAISafeBoss.bossWeight = 1;
+
+            ItemTag[] TagsScav = {
+                ItemTag.AIBlacklist,
+                ItemTag.SprintRelated,
+                ItemTag.InteractableRelated,
+                ItemTag.OnStageBeginEffect,
+                ItemTag.HoldoutZoneRelated,
+                NoMonsterScav
+            };
+            //No Equipment
+            //No Onkill
+            //Evo blacklist
+            ItemTag[] TagsMobsEvo = {
+                ItemTag.AIBlacklist,
+                ItemTag.OnKillEffect,
+                ItemTag.EquipmentRelated,
+                ItemTag.CannotCopy,
+                NoEvolSimu,
+                NoMonsterScav,
+                ItemTag.SprintRelated,
+                ItemTag.InteractableRelated,
+                ItemTag.OnStageBeginEffect,
+                ItemTag.HoldoutZoneRelated
+            };
+            ItemTag[] Arena = {
+                ItemTag.AIBlacklist,
+                ItemTag.OnKillEffect,
+                ItemTag.EquipmentRelated,
+                ItemTag.CannotCopy,
+                NoMonsterScav,
+                ItemTag.SprintRelated,
+                ItemTag.InteractableRelated,
+                ItemTag.OnStageBeginEffect,
+                ItemTag.HoldoutZoneRelated
+            };
+            dtMonsterTeamTier1Item.bannedItemTags = TagsMobsEvo;
+            dtMonsterTeamTier2Item.bannedItemTags = TagsMobsEvo;
+            dtMonsterTeamTier3Item.bannedItemTags = TagsMobsEvo;
+            dtMonsterTeamLunar.bannedItemTags = TagsMobsEvo;
+
+            dtArenaMonsterTier1.bannedItemTags = Arena;
+            dtArenaMonsterTier2.bannedItemTags = Arena;
+            dtArenaMonsterTier3.bannedItemTags = Arena;
+
+            dtAISafeTier1Item.bannedItemTags = TagsScav;
+            dtAISafeTier2Item.bannedItemTags = TagsScav;
+            dtAISafeTier3Item.bannedItemTags = TagsScav;
+            dtAISafeBoss.bannedItemTags = TagsScav;
+
+
+        }
+
+        public static void CallLate()
+        {
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.CloverVoid.tags, ItemTag.DevotionBlacklist);
+            //HG.ArrayUtils.ArrayAppend(ref DLC2Content.Items.SpeedBoostPickup.tags, ItemTag.DevotionBlacklist);
+
+
+
+            #region Tag Fixes
+
+            ArrayUtils.ArrayRemoveAtAndResize(ref RoR2Content.Items.FlatHealth.tags, 1, 1); //Remove OnkillTag
+
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.BonusGoldPackOnKill.tags, ItemTag.AIBlacklist); //Enemies cannot use Gold
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.MoveSpeedOnKill.tags, ItemTag.OnKillEffect); //Missed Tag
+           
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.MonstersOnShrineUse.tags, ItemTag.AIBlacklist);
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.GoldOnHit.tags, ItemTag.AIBlacklist);
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.LunarTrinket.tags, ItemTag.AIBlacklist);
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.FocusConvergence.tags, ItemTag.AIBlacklist);
+            ArrayUtils.ArrayAppend(ref DLC2Content.Items.OnLevelUpFreeUnlock.tags, ItemTag.AIBlacklist);
+
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.MushroomVoid.tags, ItemTag.SprintRelated); //Missed Tag
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.MushroomVoid.tags, ItemTag.AIBlacklist); //Sprint is blacklisted
+
+
+            RoR2Content.Items.ParentEgg.tags[0] = ItemTag.Healing;
+
+            #endregion
+
+
+
+            if (!WConfig.cfgItemTags.Value)
+            {
+                return;
+            }
+
+            #region AI Blacklist
+            #region White
+ 
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.FlatHealth.tags, NoEvolSimu); //Useless,
+
+            #endregion
+            #region Green
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.Infusion.tags, ItemTag.AIBlacklist); //Useless
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.PrimarySkillShuriken.tags, NoEvolSimu); //Borderline for runs, Keeping in Vfields
+
+            #endregion
+            #region Red
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.NovaOnHeal.tags, ItemTag.AIBlacklist); //Overpowered
+
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.BarrierOnOverHeal.tags, NoEvolSimu); //Useless
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.MoreMissile.tags, NoEvolSimu); //Useless
+            
+            //ArrayUtils.ArrayAppend(ref RoR2Content.Items.CaptainDefenseMatrix.tags, ItemTag.CannotSteal); //Passive shouldnt be stealable ig?
+            #endregion
+            #region Boss
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.TitanGoldDuringTP.tags, ItemTag.AIBlacklist); //Cant use
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.SprintWisp.tags, ItemTag.AIBlacklist); //Cant use
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.MinorConstructOnKill.tags, ItemTag.AIBlacklist);  //Cant use
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.SiphonOnLowHealth.tags, ItemTag.AIBlacklist); //Already on BrotherBlacklist
+
+
+            //Mithrix would also be affected by these tags so keep that in mind
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.RoboBallBuddy.tags, NoMonsterScav); //Op Confusing
+
+            #endregion
+            #region Lunar
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.LunarSun.tags, NoEvolSimu); //Op
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.LunarSun.tags, NoMonsterScav); //Op
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.RandomlyLunar.tags, ItemTag.AIBlacklist);
+
+            #endregion
+            #region Void
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.ElementalRingVoid.tags, NoEvolSimu); //Unfun
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.ExplodeOnDeathVoid.tags, NoEvolSimu); //Op
+            ArrayUtils.ArrayAppend(ref DLC1Content.Items.ExplodeOnDeathVoid.tags, NoMonsterScav); //Op
+
+            #endregion
+
+
+            #region Modded
+            ItemDef tempDef = ItemCatalog.GetItemDef(ItemCatalog.FindItemIndex("WatchMetronome"));
+            if (tempDef != null)
+            {
+                ArrayUtils.ArrayAppend(ref tempDef.tags, ItemTag.AIBlacklist);
+            }
+            tempDef = ItemCatalog.GetItemDef(ItemCatalog.FindItemIndex("VV_ITEM_EHANCE_VIALS_ITEM"));
+            if (tempDef != null)
+            {
+                ArrayUtils.ArrayAppend(ref tempDef.tags, ItemTag.AIBlacklist);
+            }
+            #endregion
+            #endregion
+
+
+
+            #region Category stuff
+            //General rule changes
+            //Barrier -> Healing : Instead of Healing+Utility
+            //Max HP -> Healing : Instead of Healing+Utility
+
+            //Some Max HP is already only tagged as Healing
+            //How Barrier is Utility? Like it's not DR 
+            //Healing is a small pool it shouldnt overlap with much other stuff
+
+            #region White
+            ArrayUtils.ArrayRemoveAtAndResize(ref RoR2Content.Items.BarrierOnKill.tags, 0); //Remove Utility
+            #endregion
+            #region Green
+            //Green
+            ArrayUtils.ArrayRemoveAtAndResize(ref RoR2Content.Items.Infusion.tags, 0); //Remove Utility
+            ArrayUtils.ArrayAppend(ref DLC2Content.Items.TeleportOnLowHealth.tags, ItemTag.Healing); //Barrier is Healing.
+            #endregion
+            #region Red
+            ArrayUtils.ArrayRemoveAtAndResize(ref RoR2Content.Items.HeadHunter.tags, 0); //Remove Utility
+            ArrayUtils.ArrayRemoveAtAndResize(ref RoR2Content.Items.BarrierOnOverHeal.tags, 0); //Remove Utility, Barrier is Healing.
+
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.NovaOnHeal.tags, ItemTag.Healing);
+            //ArrayUtils.ArrayAppend(ref DLC1Content.Items.ImmuneToDebuff.tags, ItemTag.Healing); //Already is Healing
+
+            #endregion
+            #region Boss
+            ArrayUtils.ArrayRemoveAtAndResize(ref RoR2Content.Items.Knurl.tags, 0); //Remove Utility
+
+            #endregion
+            #region Lunar
+
+            RoR2Content.Items.RandomDamageZone.tags[0] = ItemTag.Damage; //Utility???
+            DLC1Content.Items.HalfSpeedDoubleHealth.tags[0] = ItemTag.Healing; //Utility???
+            DLC1Content.Items.LunarSun.tags[0] = ItemTag.Damage; //Utility???
+            RoR2Content.Items.LunarUtilityReplacement.tags[0] = ItemTag.Healing; //This Heals you
+            ArrayUtils.ArrayAppend(ref RoR2Content.Items.ShieldOnly.tags, ItemTag.Healing); //Max HP
+            #endregion
+
+            #region Void
+            //ArrayUtils.ArrayAppend(ref DLC1Content.Items.ElementalRingVoid.tags, ItemTag.Utility); //Uh?
+
+            #endregion
+  
+            #endregion
+
+        }
+
+
+
+    }
+
+
+}
